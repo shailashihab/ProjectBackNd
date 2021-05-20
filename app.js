f456432b69972df365c35d1f2234cf8052f449b1
saranyasidharth <72602448+saranyasidharth@users.noreply.github.com>
2021-05-20 21:07:25 +0530

    second commit

--- app.js
+++ app.js
@@ -1,18 +1,69 @@
-const express = require('express');
-const app = new express;
-const mongoose = require('mongoose');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const PORT = process.env.PORT || 5000;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var cors= require('cors');
+var app = express();
 
-// Middlewares
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:4200','http://127.0.0.1:4200'],
+  credentials:true
+}));
+
+var mongoose =require('mongoose');
+
+mongoose.connect('mongodb://localhost/sesproject');
+
+//passport
+var passport = require('passport');
+var session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+app.use(session({
+  name:'myname.sid',
+  resave:false,
+  saveUninitialized:false,
+  secret:'secret',
+  cookie:{
+    maxAge:36000000,
+    httpOnly:false,
+    secure:false
+  },
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+require('./passport-config');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/users', usersRouter);
 
-app.get('/',function(req,res){
-    res.send('BackEnd is working');
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.listen(PORT,()=>{console.log('Server at 5000')});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
